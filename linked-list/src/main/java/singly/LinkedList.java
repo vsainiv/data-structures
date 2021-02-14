82c79d34a418448fa9a0ee2ce23e967440742783
Vineet Saini <vineetsaini20@gmail.com>
2021-02-14 22:45:45 +0530

    Linked list delete operations

--- linked-list/src/main/java/singly/LinkedList.java
+++ linked-list/src/main/java/singly/LinkedList.java
@@ -18,12 +18,23 @@ public class LinkedList {
     }
   }
 
+  /**
+   * Insert at start
+   *
+   * @param value
+   */
   private void insertAtFirst(int value) {
     Node node = new Node(value);
     node.next = head;
     head = node;
   }
 
+  /**
+   * Insert after the given node
+   *
+   * @param prevNode
+   * @param value
+   */
   private void insertAfter(Node prevNode, int value) {
     if (prevNode == null) {
       return;
@@ -34,6 +45,11 @@ public class LinkedList {
     prevNode.next = node;
   }
 
+  /**
+   * Insert at the end
+   *
+   * @param value
+   */
   private void insertAtEnd(int value) {
     if (head == null) {
       head = new Node(value);
@@ -47,6 +63,65 @@ public class LinkedList {
     last.next = new Node(value);
   }
 
+  /**
+   * Deletes the node with the given key
+   *
+   * @param key
+   */
+  private void deleteNode(int key) {
+    if (head.data == key) {
+      head = head.next;
+      return;
+    }
+
+    Node prev = head;
+    Node temp = head;
+    while (temp != null && temp.data != key) {
+      prev = temp;
+      temp = temp.next;
+    }
+
+    if (temp == null) {
+      System.out.printf("Key %d is not present in the linked list", key);
+      return;
+    }
+
+    prev.next = temp.next;
+  }
+
+  /**
+   * Delete node at the given position
+   *
+   * @param position
+   */
+  private void deleteNodeAtGivenPosition(int position) {
+    if (head == null) {
+      return;
+    }
+
+    if (position == 0) {
+      head = null;
+      return;
+    }
+
+    Node temp = head;
+    for (int i = 0; temp != null && i < position - 1; i++) {
+      temp = temp.next;
+    }
+
+    temp.next = temp.next.next;
+  }
+
+  /**
+   * Deletes the linked list
+   */
+  private void deleteLinkedList() {
+    head = null;
+  }
+
+  /**
+   * Prints the list
+   */
   private void printList() {
     Node temp = head;
     while (temp != null) {
@@ -54,6 +129,7 @@ public class LinkedList {
       temp = temp.next;
     }
     System.out.print("null");
+    System.out.println();
   }
 
   public static void main(String[] args) {
@@ -61,8 +137,27 @@ public class LinkedList {
     linkedList.insertAtFirst(1);
     linkedList.insertAtEnd(3);
     linkedList.insertAfter(linkedList.head, 2);
-
     System.out.println("Linked list after insertion :");
+    linkedList.printList();
+
+    linkedList.deleteNode(4);
+
+    linkedList.insertAtEnd(4);
+    System.out.println("Linked list before deletion :");
+    linkedList.printList();
+    linkedList.deleteNode(4);
+    System.out.println("Linked list after deletion :");
+    linkedList.printList();
+
+    linkedList.insertAtEnd(4);
+    System.out.println("Linked list before deletion :");
+    linkedList.printList();
+    linkedList.deleteNodeAtGivenPosition(3);
+    System.out.println("Linked list after deletion :");
+    linkedList.printList();
+
+    System.out.println("Deleting entire Linked list :");
+    linkedList.deleteLinkedList();
     linkedList.printList();
 
   }
